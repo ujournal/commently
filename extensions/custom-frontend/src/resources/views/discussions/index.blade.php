a93091f415a29e9b57ef1d46bb0c25c8c084f4d1
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2026-02-25 20:32:52 +0200

    Add user default icon

--- extensions/custom-frontend/src/resources/views/discussions/index.blade.php
+++ extensions/custom-frontend/src/resources/views/discussions/index.blade.php
@@ -126,7 +126,15 @@
                         <div class="discussion-item-header">
                             <span class="discussion-item-header-left">
                                 @if ($user)
-                                    <img src="{{ $user->attributes->avatarUrl ?? '' }}" alt="" class="discussion-item-user-avatar" loading="lazy">
+                                    @if(!empty($user->attributes->avatarUrl ?? ''))
+                                        <img src="{{ $user->attributes->avatarUrl }}" alt="" class="discussion-item-user-avatar" loading="lazy">
+                                    @else
+                                        <span class="discussion-item-user-avatar discussion-item-user-avatar--placeholder" aria-hidden="true">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" fill="currentColor"/>
+                                            </svg>
+                                        </span>
+                                    @endif
                                     <span class="discussion-item-user">
                                         {{ $user->attributes->displayName ?? $user->attributes->username ?? '' }}
                                     </span>
